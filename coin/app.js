35c04a7ef93ef95eb3c4d0dc17e6291066468375
ka1svr <64781018+prostoyboy@users.noreply.github.com>
2021-12-27 14:38:53 +0600

    Add files via upload

--- coin/app.js
+++ coin/app.js
@@ -1,10 +1,5 @@
-console.log("work")
-
-document.onkeydown=changeBG;
-function changeBG(event){
- if (!event) event = window.event;
- console.log(event, event.keyCode)
- switch(event.keyCode){
-  case 32: document.body.style.background = "#f8c9c99d";
- }
-}  
+$(document).keyup(function(e) {
+	if (e.key === "Space" || e.keyCode === 32) {
+		alert('Нажата клавиша Space');
+	}
+});
